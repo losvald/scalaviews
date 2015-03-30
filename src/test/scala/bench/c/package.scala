cfb733258532225beb74374907d8180289786197
Leo Osvald <leo.osvald@gmail.com>
2015-03-30 16:22:14 -0400

    Copy paste from dslapi.

--- src/test/scala/bench/c/package.scala
+++ src/test/scala/bench/c/package.scala
@@ -44,6 +44,76 @@ package object bench {
         case "Array[Double]" => "double*"
         case _ => super.remap(m)
       }
+
+      // XXX: the rest is copied from dslapi.scala in LMS tutorial
+
+      def getMemoryAllocString(count: String, memType: String): String = {
+        "(" + memType + "*)malloc(" + count + " * sizeof(" + memType + "));"
+      }
+      override def format(s: Exp[Any]): String = {
+        remap(s.tp) match {
+          case "uint16_t" => "%c"
+          case "bool" | "int8_t" | "int16_t" | "int32_t" => "%d"
+          case "int64_t" => "%ld"
+          case "float" | "double" => "%f"
+          case "string" => "%s"
+          case "char*" => "%s"
+          case "char" => "%c"
+          case "void" => "%c"
+          case _ =>
+            throw new scala.virtualization.lms.internal.
+              GenerationFailedException(
+                "CGenMiscOps: cannot print type " + remap(s.tp))
+        }
+      }
+      override def quoteRawString(s: Exp[Any]): String = {
+        remap(s.tp) match {
+          case "string" => quote(s) + ".c_str()"
+          case _ => quote(s)
+        }
+      }
+      override def isPrimitiveType(tpe: String) : Boolean = {
+        // treat string as a primitive type to prevent memory management
+        // (always stack allocated and freed automatically at the scope exit)
+        tpe match {
+          case "char*" => true
+          case "char" => true
+          case _ => super.isPrimitiveType(tpe)
+        }
+      }
+      override def quote(x: Exp[Any]) = x match {
+        // TODO: more escapes?
+        case Const(s: String) => "\""+s.replace("\"", "\\\"")+"\""
+        case Const('\n') if x.tp == manifest[Char] => "'\\n'"
+        case Const('\t') if x.tp == manifest[Char] => "'\\t'"
+        case Const('\0') if x.tp == manifest[Char] => "'\\0'"
+        case _ => super.quote(x)
+      }
+      override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+        case a@ArrayNew(n) =>
+          val arrType = remap(a.m)
+          stream.println(arrType + "* " + quote(sym) + " = " +
+            getMemoryAllocString(quote(n), arrType))
+        case ArrayApply(x,n) => emitValDef(sym, quote(x) + "[" + quote(n) + "]")
+        case ArrayUpdate(x,n,y) =>
+          stream.println(quote(x) + "[" + quote(n) + "] = " + quote(y) + ";")
+        case PrintLn(s) =>
+          stream.println("printf(\"" + format(s) + "\\n\"," +
+            quoteRawString(s) + ");")
+        case StringCharAt(s,i) =>
+          emitValDef(sym, "%s[%s]".format(quote(s), quote(i)))
+        // case Comment(s, verbose, b) =>
+        //   stream.println("//#" + s)
+        //   if (verbose) {
+        //     stream.println("// generated code for " + s.replace('_', ' '))
+        //   } else {
+        //     stream.println("// generated code")
+        //   }
+        //   emitBlock(b)
+        //   emitValDef(sym, quote(getBlockResult(b)))
+        //   stream.println("//#" + s)
+        case _ => super.emitNode(sym,rhs)
+      }
     }
     val codegen = new Codegen {}
   }
